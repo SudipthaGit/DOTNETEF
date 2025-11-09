d4ffdd3baad5f1c8b9a68517ddd96d6200d38d40
SudipthaGit <sarkarsudiptha@gmail.com>
2025-11-09 14:02:14 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using EFDB.DataBase;
+using EFDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDB
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //FetchUserData();
+            
+        }
+
+        static List<UserEntity> FetchUserData()
+        {
+            try
+            {
+                //var DB = new CSDbContext();
+                using (var DB = new CSDbContext()) 
+                {
+                    var res = (from i in DB.T_user_details
+                               where i.Is_Active
+                               select new UserEntity
+                               {
+                                   user_name = i.u_name,
+                                   user_dob = i.u_dob,
+                                   user_city = i.u_city,
+                               }).ToList();
+                    return res;
+                }
+            }
+            catch (Exception e) 
+            {
+            }
+            return null;
+        }
+
+        static void FetchOrderDetails()
+        {
+
+        }
+
+    }
+}
